a66b981fe5fe89af4c8b175e0dc6ab6d044def65
zsalab-oe <gaspar.balazs@uni-obuda.hu>
2026-06-24 18:03:29 +0200

    fix game events topic

--- Assets/Scripts/Logger.cs
+++ Assets/Scripts/Logger.cs
@@ -83,7 +83,7 @@ namespace AiSims
             }
         }
 
-        public static void LogToMqtt(GameEventType eventType, string message, string topic = "game/logs/events")
+        public static void LogToMqtt(GameEventType eventType, string message, string topic = "game/events")
         {
             // 1. Create the JSON structure
             GameEventLog logEvent = new GameEventLog(eventType, message);
